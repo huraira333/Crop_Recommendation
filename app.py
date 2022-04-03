54968a46c0d48692fd6ea11d1fdb268d323f2d6d
huraira333 <102724415+huraira333@users.noreply.github.com>
2022-04-03 21:58:11 +0500

    Create app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 import numpy as np
-from flask import Flask, request,render_template
+from flask import Flask, request,render_template,jsonify
 import pickle
 
-app = Flask(__name__)
+
 model = pickle.load(open('model_fertilizer.pkl', 'rb'))
+app = Flask(_name_)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
+@app.route('/',methods=['GET'])
 def predict():
     
+    nitrogen = int(request.args['nitrogen'])
+    phosphorus = int(request.args['phosphorus'])
+    potassium = int(request.args['potassium'])
+    temprature = int(request.args['temprature'])
+    humidity = int(request.args['humidity'])
+    ph = int(request.args['ph'])
+    rainfall = int(request.args['rainfall'])
+    pred = model.predict(np.array([nitrogen,phosphorus,potassium,temprature,humidity,ph,rainfall]).reshape(1,-1))
+    return jsonify(prediction = str(pred))
 
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    final_features=np.reshape(final_features, (-1, 1)).T
-    prediction = model.predict(final_features)
-    return render_template('index.html', prediction_text='Predicted Crop is {}'.format(prediction))
 
 
-
-if __name__ == "__main__":
+if _name_ == "_main_":
     app.run(debug=True)
