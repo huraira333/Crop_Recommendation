3a4bf443a0dad8148d3028809a491e2bddbc9f58
huraira333 <102724415+huraira333@users.noreply.github.com>
2022-04-03 22:23:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import pickle
 model = pickle.load(open('model_fertilizer.pkl', 'rb'))
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/',methods=['GET'])
 def predict():
